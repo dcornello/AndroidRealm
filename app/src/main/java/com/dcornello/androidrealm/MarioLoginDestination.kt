11328b3ce2629c73b161e2e2677c4fa8332249dc
Mario Rossano <mario.rossano@treatwell.com>
2024-05-28 12:23:03 +0200

    side effect

--- app/src/main/java/com/dcornello/androidrealm/MarioLoginDestination.kt
+++ app/src/main/java/com/dcornello/androidrealm/MarioLoginDestination.kt
@@ -1,21 +1,28 @@
 package com.dcornello.androidrealm
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.navigation.NavHostController
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.collect
 
 @Composable
 fun MarioLoginDestination(navHostController: NavHostController, viewModel: MarioLoginScreenViewModel) {
     val stateStream by viewModel.uiState.collectAsState()
-
+    LaunchedEffect(viewModel) {
+        viewModel.sideEffects.onEach {sideEffect ->
+            when(sideEffect) {
+                is MarioLoginScreenViewModel.SideEffects.NavigateToPage1 -> navHostController.navigate("page1/${sideEffect.email}")
+            }
+        }.collect()
+    }
     MarioLoginScreen(
         uiState = stateStream,
         onEmailChange = viewModel::changeEmail,
         onPasswordChange = viewModel::passwordChange,
-        onLoginTap = {
-            navHostController.navigate("page1/${stateStream.email}")
-        },
+        onLoginTap = viewModel::navigateToPage1,
         onTooglePasswordVisibilityTap = viewModel::tooglePasswordVisibility
     )
 }
--- app/src/main/java/com/dcornello/androidrealm/MarioLoginScreenViewModel.kt
+++ app/src/main/java/com/dcornello/androidrealm/MarioLoginScreenViewModel.kt
@@ -1,14 +1,23 @@
 package com.dcornello.androidrealm
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharedFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 
 class MarioLoginScreenViewModel: ViewModel() {
     private val _uiState = MutableStateFlow(LoginScreenUIState(passwordIsVisible = false, email = "", password = ""))
     val uiState: StateFlow<LoginScreenUIState> = _uiState.asStateFlow()
+
+    private val _sideEffects = MutableSharedFlow<SideEffects>()
+    val sideEffects: Flow<SideEffects> = _sideEffects.asSharedFlow()
 
     fun changeEmail(email: String) {
         _uiState.update {
@@ -27,4 +36,19 @@ class MarioLoginScreenViewModel: ViewModel() {
             it.copy(passwordIsVisible = it.passwordIsVisible.not())
         }
     }
+
+    fun navigateToPage1() {
+        sendSideEffect(SideEffects.NavigateToPage1(email = _uiState.value.email))
+    }
+    private fun sendSideEffect(sideEffects: SideEffects) {
+        viewModelScope.launch {
+            _sideEffects.emit(sideEffects)
+        }
+    }
+
+    sealed class SideEffects {
+        data class NavigateToPage1(val email: String): SideEffects()
+
+    }
+
 }
